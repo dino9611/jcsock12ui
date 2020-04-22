f3b39f05bd3876171e911dcd2af3ba11c26511f6
Dino <aldinorahman36@gmail.com>
2020-04-22 11:53:10 +0700

    dadsa

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component } from 'react';
+import io from 'socket.io-client';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    messages:[],
+    usercount:0
+  }
 
+  componentDidMount(){
+    const socket = io('http://localhost:2020/',{transports: [ 'websocket' ]});
+
+    socket.on('chat message', this.updateMessages);
+    socket.on('user connected', (count)=>this.setState({usercount:count}));
+
+    axios.get('http://localhost:2020/chat/getmessages')
+    .then((res) => {
+      this.setState({messages:res.data})
+    })
+  }
+
+
+  updateMessages = (msgs) => {
+    this.setState({messages:msgs})
+  }
+
+  onBtnSendClick = () => {
+    axios.post('http://localhost:2020/chat/sendmessage', {
+      nama: this.refs.nama.value,
+      message: this.refs.message.value
+    }).then((res) => {
+      console.log(res.data)
+    })
+  }
+
+  onBtnClearClick = () => {
+    axios.delete('http://localhost:2020/chat/clearmessages')
+    .then((res) => {
+      console.log(res.data)
+    })
+  }
+
+  renderListMessage = () => {
+    return this.state.messages.map((item, index) => {
+      return (
+        <tr key={index}>
+          <td>{item.nama}</td>
+          <td>{item.message}</td>
+          <td></td>
+        </tr>
+      )
+    })
+  }
+
+  pencet=()=>{
+    // const socket = io('http://localhost:2020/',{transports: [ 'websocket' ]});
+    // socket.emit('tes','blalaaa')
+  }
+
+  render() { 
+    return (
+      <center>
+      <h2>Chat Group (User Connected : {this.state.usercount})</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Nama</th>
+            <th>Message</th>
+            <th><input type="button" value="Clear" onClick={this.onBtnClearClick} /></th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.renderListMessage()}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td>
+              <input type="text" ref='nama' />
+            </td>
+            <td>
+              <input type="text" ref='message' />
+            </td>
+            <td>
+              <input type="button" value="Send" onClick={this.onBtnSendClick} />
+              <button onClick={this.pencet}>dasd</button>
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    </center>
+      );
+  }
+}
+ 
 export default App;
